a167fb6195bb1fc87fde53f66b6dd761b90c6743
Özkan Bölükbaş <ozkanbolukbas@gmail.com>
2018-01-04 13:03:34 +0300

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 
 const app = express();
-const port=3000;
+const port= process.env.PORT || 3000;
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
